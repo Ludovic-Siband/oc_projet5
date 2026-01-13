5f1dea4af6c312186665aac9cea4129a1c1caf49
bancs6 <ludovic.siband@gmail.com>
2026-01-13 17:09:06 +0100

    feat(front): add auth guard and split auth/app layouts

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
   {
@@ -8,18 +9,37 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: '',
+    path: 'login',
     loadComponent: () =>
-      import('./layouts/app-layout/app-layout.component').then((module) => module.AppLayoutComponent),
+      import('./layouts/auth-layout/auth-layout.component').then((module) => module.AuthLayoutComponent),
     children: [
       {
-        path: 'login',
+        path: '',
         loadComponent: () => import('./pages/login/login.component').then((module) => module.LoginComponent),
       },
+    ],
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./layouts/auth-layout/auth-layout.component').then((module) => module.AuthLayoutComponent),
+    children: [
       {
-        path: 'register',
+        path: '',
         loadComponent: () =>
           import('./pages/register/register.component').then((module) => module.RegisterComponent),
+      },
+    ],
+  },
+  {
+    path: 'feed',
+    loadComponent: () =>
+      import('./layouts/app-layout/app-layout.component').then((module) => module.AppLayoutComponent),
+    canMatch: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/feed/feed.component').then((module) => module.FeedComponent),
       },
     ],
   },
